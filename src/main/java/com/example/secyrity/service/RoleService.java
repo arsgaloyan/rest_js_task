7fa779ca983c084a16af71366d66a793aa6f5a35
arsgaloyan <139362615+arsgaloyan@users.noreply.github.com>
2023-10-10 19:08:54 +0300

    update

--- src/main/java/com/example/secyrity/service/RoleService.java
+++
@@ -1,9 +0,0 @@
-package com.example.secyrity.service;
-
-import com.example.secyrity.model.Role;
-
-import java.util.List;
-public interface RoleService {
-    Role getRoleByName(String name);
-    List<Role> getListOfRoles();
-}
